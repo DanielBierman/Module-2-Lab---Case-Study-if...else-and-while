5fcd16e2bdb3db0b238fb6d3517e7bd068e8ab6c
Daniel Bierman <dbiermam1@ivytech.edu>
2024-11-04 13:51:46 -0500

    M02 Lab created

---
+++ M02CaseStudy.py
@@ -0,0 +1,19 @@
+last_name = ""
+first_name = ""
+gpa = 0.0
+
+while last_name != "ZZZ":
+    last_name = input("Please enter student's last name or 'ZZZ' to quit: ")
+    if last_name == "ZZZ":
+        break
+    first_name = input("Please enter student's first name: ")
+    gpa = float(input(f"What is {first_name} {last_name}'s GPA "))
+    
+    if gpa >= 3.5:
+        print(f"{first_name} {last_name} has made the Dean's List!")
+    elif gpa >= 3.25 and gpa < 3.5:
+        print (f"{first_name} {last_name} has made the Honor Roll!")
+    else: 
+        print(f"{first_name} {last_name} did not make the Dean's list or Honor Roll.")
+    
+    
